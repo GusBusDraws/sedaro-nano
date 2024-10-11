7fead5b9738246a0ed28270b2d83021e3e39c052
cgusb <cgusbecker@gmail.com>
2024-10-11 13:15:51 -0400

    Add test for 'processData' with mock simulation data

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ The tiniest possible mockup of our system
 ## Notes
 
 ## Change Log
+- Add test for `processData` with mock simulation data
 - Move data processing to `processData` in `dataUtils.js`
 - Increase heap size to prevent browser shut down
 
--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,8 +1,57 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { processData } from "./dataUtils";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('correctly transforms data for Plotly.js', () => {
+  const data = [
+    [
+      -99,
+      0,
+      {
+        "Planet": {
+          "time": 0,
+          "x": 0,
+          "y": 0.1
+        },
+        "Satellite": {
+          "time": 0,
+          "x": 0,
+          "y": 1
+        }
+      }
+    ],
+    [
+      0,
+      0.01,
+      {
+        "Planet": {
+          "time": 0.01,
+          "x": 0.001,
+          "y": 0.1
+        }
+      }
+    ],
+    [
+      0,
+      0.01,
+      {
+        "Satellite": {
+          "time": 0.01,
+          "x": 0.01,
+          "y": 0.1
+        }
+      }
+    ]
+  ];
+
+  const processed = processData(data);
+
+  expect(processed).toEqual({
+    'Planet': {
+      x: [0, 0.001],
+      y: [0.1, 0.1]
+    },
+    'Satellite': {
+      x: [0, 0.01],
+      y: [1, 0.1]
+    }
+  });
 });
